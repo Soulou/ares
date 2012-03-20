202d78f177d8e413fd4d2e39ace681c44d612f69
Soulou <leo.unbek@gmail.com>
2012-03-20 20:43:17 +0100

    Initialisation de la bdd avec rails runner script/init_users.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,6 @@
-require 'csv'
-
 admin_group = Group.create name: 'admin'
 student_group = Group.create name: 'student'
 
-data = CSV.read("/home/leo/Projets/ares/db/ENSIIE.csv")
-
-data.each do |line|
-  if line[2] == nil then
-    puts "Nom obligatoire"
-    exit
-  end
-  if line[3] == nil then
-    puts "Prenom obligatoire"
-    exit
-  end
-  if line[6] == nil then
-    puts "Email obligatoire"
-    exit
-  end
-  if line[0] == nil then
-    puts "Annee d'entree obligatoire"
-    exit
-  end
-  if line[1] == nil then
-    puts "Promotion obligatoire"
-    exit
-  end
-
-  User.create username: line[2].mb_chars.normalize(:kd).downcase.strip.gsub(" ","").tr('-', '_').chomp + line[0],
-              group: student_group,
-              password: "coucou",
-              email: line[6],
-              first_name: line[3],
-              last_name: line[2],
-              promotion: line[1],
-              start_year: line[0]
-end
+User.create username: "leo", group: admin_group,
+  password: "leo", email: "leo@unbekandt.eu", first_name: "Léo",
+  last_name: "Unbekandt", promotion: 2014, start_year: 2011
---
+++ script/init_users.rb
@@ -0,0 +1,40 @@
+require 'csv'
+
+admin_group = Group.create name: 'admin'
+student_group = Group.create name: 'student'
+
+data = CSV.read("/home/leo/Projets/ares/db/ENSIIE.csv")
+
+data.each do |line|
+  if line[2] == nil then
+    puts "Nom obligatoire"
+    exit
+  end
+  if line[3] == nil then
+    puts "Prenom obligatoire"
+    exit
+  end
+  if line[6] == nil then
+    puts "Email obligatoire"
+    exit
+  end
+  if line[0] == nil then
+    puts "Annee d'entree obligatoire"
+    exit
+  end
+  if line[1] == nil then
+    puts "Promotion obligatoire"
+    exit
+  end
+
+  username = line[2].mb_chars.normalize(:kd).downcase.strip.gsub(" ","").tr('-', '_').tr('é', 'e').chomp + line[0]
+  User.create username: username,
+              group: student_group,
+              password: "coucou",
+              email: line[6],
+              first_name: line[3],
+              last_name: line[2],
+              promotion: line[1],
+              start_year: line[0]
+
+end
